657d662848cd6187e81d0c9b2d8a5268bc35d099
Raskollnikov <bakhalaia56@gmail.com>
2024-10-02 21:11:20 +0400

    cors fixed

--- build/index.js
+++ build/index.js
@@ -12,7 +12,11 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 app.use(express_1.default.json());
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
 app.use("/financial-record", financial_records_1.default);
 const mongoURI = process.env.MONGO_URI;
 mongoose_1.default
